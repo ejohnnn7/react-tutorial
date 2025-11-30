7ea6a095e9ab8780ea56b1128df9d249ccaec263
Elizabeth John <ejohnnn7@gmail.com>
2025-11-30 16:44:47 -0500

    React Tutorial Completed

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { use, useEffect, useReducer } from 'react';
+import './App.css';
+import chef from "./images/chef.jpg";
+
+function Header({ name, year }) {
+  return (
+    <header>
+      <h1>{name}'s Kitchen</h1>
+      <p>Copyright {year}</p>
+    </header>
+  );
+}
+
+const items = ["Apples", "Bananas", "Cherries", "Dates"];
+
+const dishObjects = items.map((dish, i) => ({
+  id: i,
+  title: dish,
+}));
+
+function Main({ dishes, openStatus, onStatus }) {
+  return (
+    <>
+      <div>
+        <button onClick={() => onStatus(true)}>
+          I want to be open
+        </button>
+
+        <h2>
+          Welcome to this restaurant!{" "}
+          {openStatus ? "Open" : "Closed"}
+        </h2>
+      </div>
+
+      <main>
+        <img src={chef} height={200} alt="A photo of a chef" />
+        <ul>
+          {dishes.map((dish) => (
+            <li key={dish.id} style={{ listStyleType: "none" }}>
+              {dish.title}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
+  );
+}
+
+function App() {
+  const [status, toggle] = useReducer(
+    (status) => !status,
+    true
+  );
+
+  useEffect(() => {
+    console.log(`The restaurant is now ${status ? "Open" : "Closed"}`)
+  }, [status]);
+
+  return (
+    <div>
+      <h1>
+        The restaurant is currently {status ? "Open" : "Closed"}
+      </h1>
+
+      <button onClick={toggle}>
+        {status ? "Close Restaurant" : "Open Restaurant"}
+      </button>
+
+      <Header name="Alex" year={new Date().getFullYear()} />
+
+      <Main
+        dishes={dishObjects}
+        openStatus={status}
+        onStatus={toggle}
+      />
+    </div>
+  );
+}
+
+export default App;
